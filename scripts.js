6f9166f4d0421ad6523904368a73438be4ce6f4f
Kahlil Gersbach <kahlilgersbach@gmail.com>
2018-10-28 18:40:49 -0400

    webcam with canvas working

--- scripts.js
+++ scripts.js
@@ -3,3 +3,38 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
+}
+
+// need to make sure video is the same size ass the canvas.
+function paintToCanvas() {
+  const width = video.videoWidth
+  const height = video.videoHeight
+  canvas.width = width;
+  canvas.height = height;
+  // use return so if i ever need access to the interval, 
+  // I have it. clearInterval()
+  return setInterval(() => {
+    // pass image or vid, start at 0,0 (top left screen)
+    // then paint width and height.
+    // that is why we stuck them in there own separate variables.
+    ctx.drawImage(video, 0, 0, width, height)
+  }, 16);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+}
+
+getVideo();
